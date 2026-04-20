f69fbeac7dbbd4d2280d8d9d8f194444bd5c302e
DataMundi <datamundi.lab@gmail.com>
2026-04-20 13:12:19 -0300

    fix: namespaces corretos para sysfonts e showtext

--- R/zzz.R
+++ R/zzz.R
@@ -5,5 +5,5 @@
 .onLoad <- function(libname, pkgname) {
   sysfonts::font_add_google("Montserrat", "montserrat")
   sysfonts::font_add_google("Fira Code",  "fira_code")
-  sysfonts::showtext_auto()
+  showtext::showtext_auto()
 }
