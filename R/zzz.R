1415514f0cbc7575a44b6a5610edd43b490e6a2c
DataMundi <datamundi.lab@gmail.com>
2026-04-20 13:10:59 -0300

    fix: font_add_google movido para sysfonts

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
 # ============================================================
 
 .onLoad <- function(libname, pkgname) {
-  showtext::font_add_google("Montserrat", "montserrat")
-  showtext::font_add_google("Fira Code",  "fira_code")
-  showtext::showtext_auto()
+  sysfonts::font_add_google("Montserrat", "montserrat")
+  sysfonts::font_add_google("Fira Code",  "fira_code")
+  sysfonts::showtext_auto()
 }
